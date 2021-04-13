6a3ace023f99fad4210c4aa164f8950551441fba
shengchunqiang <scqzyq@gmail.com>
2021-04-13 11:08:31 +0800

    init

---
+++ src/main/java/com/scqzyq/tearalgorithm/TearAlgorithmApplication.java
@@ -0,0 +1,13 @@
+package com.scqzyq.tearalgorithm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TearAlgorithmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TearAlgorithmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/scqzyq/tearalgorithm/TearAlgorithmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scqzyq.tearalgorithm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TearAlgorithmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
