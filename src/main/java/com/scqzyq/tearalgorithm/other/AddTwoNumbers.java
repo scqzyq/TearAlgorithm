42306bbb2286302b5084485f263c9f179dc4091e
shengchunqiang <scqzyq@gmail.com>
2021-04-25 17:40:25 +0800

    stack other

--- src/main/java/com/scqzyq/tearalgorithm/other/AddTwoNumbers.java
+++ src/main/java/com/scqzyq/tearalgorithm/other/AddTwoNumbers.java
@@ -12,13 +12,13 @@ import java.util.Objects;
 public class AddTwoNumbers {
 
     public static void main(String[] args) {
-        ListNode node1 = new ListNode(0);
-//        node1.next = new ListNode(4);
-//        node1.next.next = new ListNode(3);
+        ListNode node1 = new ListNode(2);
+        node1.next = new ListNode(4);
+        node1.next.next = new ListNode(3);
 
-        ListNode node2 = new ListNode(0);
-//        node2.next = new ListNode(6);
-//        node2.next.next = new ListNode(4);
+        ListNode node2 = new ListNode(5);
+        node2.next = new ListNode(6);
+        node2.next.next = new ListNode(4);
 
 
         ListNode node = addTwoNumbers(node1, node2);
@@ -27,7 +27,35 @@ public class AddTwoNumbers {
     }
 
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-
+        ListNode head = new ListNode(-1);
+        ListNode node = head;
+        int ten = 0;
+        while (Objects.nonNull(l1) && Objects.nonNull(l2)) {
+            node.next = new ListNode((ten + l1.val + l2.val) % 10);
+            node = node.next;
+            if (ten + l1.val + l2.val > 9) {
+                ten = 1;
+            } else {
+                ten = 0;
+            }
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+        ListNode tmp = Objects.isNull(l1) ? l2 : l1;
+        while (Objects.nonNull(tmp)) {
+            node.next = new ListNode((ten + tmp.val) % 10);
+            node = node.next;
+            if (tmp.val + ten > 9) {
+                ten = 1;
+            } else {
+                ten = 0;
+            }
+            tmp = tmp.next;
+        }
+        if (ten == 1) {
+            node.next = new ListNode(1);
+        }
+        return head.next;
     }
 
 }
--- src/main/java/com/scqzyq/tearalgorithm/stack/Calculate.java
+++ src/main/java/com/scqzyq/tearalgorithm/stack/Calculate.java
@@ -1,5 +1,8 @@
 package com.scqzyq.tearalgorithm.stack;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 /**
  * @Description: 基本计算器 LeetCode 224
  * @Author: 盛春强
@@ -7,8 +10,45 @@ package com.scqzyq.tearalgorithm.stack;
  */
 public class Calculate {
 
-    public int calculate(String s) {
+    public static void main(String[] args) {
+        String s = "1 + 1";
+        System.out.println(calculate(s));
+    }
 
-        return 0;
+    public static int calculate(String s) {
+        Deque<Integer> d = new ArrayDeque<>();
+        Deque<Integer> dt = new ArrayDeque<>();
+        char[] chars = s.replace(" ", "").toCharArray();
+        int sign = 1;
+        int res = 0;
+        for (char c : chars) {
+            if (Character.isDigit(c)) {
+                if (dt.isEmpty()) {
+                    dt.push(c - '0');
+                } else {
+                    dt.push(dt.pop() * 10 + (c - '0'));
+                }
+            } else {
+                if (!dt.isEmpty()) {
+                    res += sign * dt.pop();
+                }
+            }
+            if (c == '(') {
+                d.push(res);
+                d.push(sign);
+                res = 0;
+                sign = 1;
+            } else if (c == '+') {
+                sign = 1;
+            } else if (c == '-') {
+                sign = -1;
+            } else if (c == ')') {
+                res = d.pop()*res + d.pop();
+            }
+        }
+        if (!dt.isEmpty()) {
+            res += sign * dt.pop();
+        }
+        return res;
     }
 }
