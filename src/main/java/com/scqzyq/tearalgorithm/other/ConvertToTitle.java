3d6aa731080cb10ee8902966b503484b004a36a4
scqzyq <scqzyq@gmail.com>
2021-06-29 11:58:15 +0800

    进制转换，中位数

---
+++ src/main/java/com/scqzyq/tearalgorithm/other/ConvertToTitle.java
@@ -0,0 +1,18 @@
+package com.scqzyq.tearalgorithm.other;
+
+public class ConvertToTitle {
+
+    public static String convertToTitle(int columnNumber) {
+        StringBuilder rt = new StringBuilder();
+        while (columnNumber != 0) {
+            rt.append((char) ('A'+ --columnNumber % 26));
+            columnNumber /= 26;
+        }
+        return rt.reverse().toString();
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(convertToTitle(701));
+    }
+}
---
+++ src/main/java/com/scqzyq/tearalgorithm/other/FindMedianSortedArrays.java
@@ -0,0 +1,46 @@
+package com.scqzyq.tearalgorithm.other;
+
+public class FindMedianSortedArrays {
+
+    public static void main(String[] args) {
+        double medianSortedArrays = findMedianSortedArrays(new int[]{1, 2}, new int[]{-1, 3});
+        System.out.println(medianSortedArrays);
+    }
+
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int[] tmp;
+        if (nums1.length == 0 || nums2.length == 0) {
+            tmp = simpleSum(nums1, nums2);
+        } else if (nums1[nums1.length - 1] < nums2[0]) {
+            tmp = simpleSum(nums1, nums2);
+        } else if (nums2[nums2.length - 1] < nums1[0]) {
+            tmp = simpleSum(nums2, nums1);
+        } else {
+            tmp = simpleSum(nums1, nums2);
+            for (int i = nums1.length; i < tmp.length; i++) {
+                int j = i - 1;
+                int value = tmp[i];
+                for (; j >= 0; j--) {
+                    if (value < tmp[j]) {
+                        tmp[j + 1] = tmp[j];
+                    } else {
+                        break;
+                    }
+                }
+                tmp[j + 1] = value;
+            }
+        }
+        if (tmp.length % 2 == 1) {
+            return tmp[tmp.length / 2];
+        } else {
+            return (tmp[tmp.length / 2] + tmp[tmp.length / 2 - 1]) / 2.0;
+        }
+    }
+
+    private static int[] simpleSum(int[] nums1, int[] nums2) {
+        int[] tmp = new int[nums1.length + nums2.length];
+        System.arraycopy(nums1, 0, tmp, 0, nums1.length);
+        System.arraycopy(nums2, 0, tmp, nums1.length, nums2.length);
+        return tmp;
+    }
+}
